ba8d70d5fd30c924d0e2886d23bc2c806a44b4bd
좌 준협 <jwajunhyeob@jwaui-MacBook-Air.local>
2018-08-12 18:39:30 +0900

    What is the Cookies?

--- cookie.js
+++ cookie.js
@@ -1,8 +1,23 @@
 var express = require("express");
+var cookieParser = require("cookie-parser");
 var app = express();
 
+//middleware
+app.use(cookieParser());
+
 app.get("/count", (req, res) => {
-  res.send("count : ");
+  if (req.cookies.count) {
+    var counter = req.cookies.count;
+  } else {
+    var counter = 0;
+  }
+  counter = parseInt(counter) + 1;
+  res.cookie("count", counter);
+  res.send("count : " + counter);
+});
+
+app.get("/products", (req, res) => {
+  res.send("products");
 });
 
 app.listen(8081, () => {
