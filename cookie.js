5689ab18462858d1228c98069694f6f8a5f705da
좌 준협 <jwajunhyeob@jwaui-MacBook-Air.local>
2018-08-12 23:50:23 +0900

    shopping cart with cookies

--- cookie.js
+++ cookie.js
@@ -5,6 +5,17 @@ var app = express();
 //middleware
 app.use(cookieParser());
 
+//settings
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+const products = [
+  { id: 1, name: "shirts" },
+  { id: 2, name: "pants" },
+  { id: 3, name: "jacket" },
+  { id: 4, name: "shoes" }
+];
+
 app.get("/count", (req, res) => {
   if (req.cookies.count) {
     var counter = req.cookies.count;
@@ -17,7 +28,51 @@ app.get("/count", (req, res) => {
 });
 
 app.get("/products", (req, res) => {
-  res.send("products");
+  res.render("products", { products: products });
+});
+
+app.get("/cart", (req, res) => {
+  if (!req.cookies.cart) {
+    res.send("Empty!");
+  }
+  var cart = req.cookies.cart;
+
+  res.render("cart", { products: products, cart: cart });
+});
+
+app.get("/cart/:id", (req, res) => {
+  var id = req.params.id;
+  if (!req.cookies.cart) {
+    var cart = {};
+  } else {
+    var cart = req.cookies.cart;
+  }
+
+  if (!cart[id]) {
+    cart[id] = 0;
+  } else {
+    cart[id] = parseInt(cart[id]);
+  }
+
+  cart[id] += 1;
+  res.cookie("cart", cart);
+  res.redirect("/cart");
+});
+
+app.get("/cart/delete/:id", (req, res) => {
+  var id = req.params.id;
+  if (req.cookies.cart) {
+    var cart = req.cookies.cart;
+    if (cart[id] >= 1) {
+      cart[id] = parseInt(cart[id]) - 1;
+    } else if (cart[id] === 0) {
+      cart[id] = null;
+    }
+
+    res.cookie("cart", cart);
+  }
+
+  res.redirect("/cart");
 });
 
 app.listen(8081, () => {
